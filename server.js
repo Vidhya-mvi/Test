eb03db20543061fa06605e1409e0502e86ff35fa
vidhya.m <vidhyam.stackup@gmail.com>
2025-06-19 08:41:30 +0530

    the cors connection

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", 
-    credentials: true,           
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  origin:     ["http://localhost:5173","https://frontend-test-i1s2.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
 
 connectDB();
