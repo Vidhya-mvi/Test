f42d599902c777bd9c87c74b8094e82b103a41ba
vidhya.m <vidhyam.stackup@gmail.com>
2025-06-19 05:37:35 +0530

    mongoos

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  mongoose.connect(process.env.MONGO_URI, {
+    
   })
     .then(() => {
       console.log('MongoDB Connected');
